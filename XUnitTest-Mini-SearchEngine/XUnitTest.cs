3388e317ce79123edf43d1cc04cebe1695baa21a
Aleksey Shapkin <ceylonneck@gmail.com>
2025-11-30 00:17:41 +0500

    feat(XUnitTest): added XUnitTest

---
+++ XUnitTest-Mini-SearchEngine/XUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using Mini_SearchEngine;
+
+namespace XUnitTest_Mini_SearchEngine
+{
+    public class XUnitTest
+    {
+        [Theory]
+        [InlineData(@"C:\Users\Broky\OneDrive\Desktop\Тесты для поискового движка\Проверка изменения пути\Первый тест.txt")]
+        [InlineData("C:\\Users\\Broky\\OneDrive\\Desktop\\Тесты для поискового движка\\Проверка изменения пути\\Второй тест.txt")]
+        [InlineData("C:/Users/Broky/OneDrive/Desktop/Тесты для поискового движка/Проверка изменения пути/Третий тест.txt")]
+        [InlineData("C://Users//Broky//OneDrive//Desktop//Тесты для поискового движка//Проверка изменения пути//Четвёртый тест.txt")]
+        [InlineData("\"C:/Users/Broky/OneDrive/Desktop/Тесты для поискового движка/Проверка изменения пути/Пятый тест.txt\"")]
+        public void FileReadIsCorrect(string path)
+        {
+            Assert.NotNull(TextSource.FromFile(path));
+        }
+    }
+}
