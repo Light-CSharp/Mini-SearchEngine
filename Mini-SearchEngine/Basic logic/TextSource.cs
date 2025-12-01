45f2d1309e24ef29c5e2ae04f1debfca78f1edaf
Aleksey Shapkin <ceylonneck@gmail.com>
2025-12-01 20:17:26 +0500

    fix(TextSource): added check for an empty path

--- Mini-SearchEngine/Basic logic/TextSource.cs
+++ Mini-SearchEngine/Basic logic/TextSource.cs
@@ -10,6 +10,12 @@ namespace Mini_SearchEngine
         /// <param name="path">Путь к файлу, откуда будет браться текст.</param>
         public static string? FromFile(string path)
         {
+            if (string.IsNullOrEmpty(path))
+            {
+                MessageAssistant.RedMessage("Пустой путь файла!");
+                return null;
+            }
+
             // Изменяем путь, убирая из него кавычки и изменяя сплеши, чтобы просчитать файл и достать из него текст.
             path = path.Trim().Replace("\"", "").Replace(@"\", "/");
             if (!File.Exists(path))
