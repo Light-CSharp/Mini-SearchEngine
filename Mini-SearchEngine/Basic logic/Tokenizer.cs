c750ff442555be0617205e4e7ff1db729e606207
Aleksey Shapkin <ceylonneck@gmail.com>
2025-12-03 21:03:07 +0500

    feat(Tokenizer): added Tokenizer class

---
+++ Mini-SearchEngine/Basic logic/Tokenizer.cs
@@ -0,0 +1,21 @@
+﻿namespace Mini_SearchEngine.Basic_logic
+{
+    public static class Tokenizer
+    {
+        /// <summary>
+        /// Возвращает массив токенов, чтобы движок обработал слова.
+        /// </summary>
+        /// <param name="text">Текст, который будет разбит на токены.</param>
+        /// <returns></returns>
+        public static string[] GetTokens(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                MessageAssistant.RedMessage("Пустой текст на этапе получения токенов!");
+                return [];
+            }
+
+            return text.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
