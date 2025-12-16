d0583c0efde7b12bec1e28393ddf6db5333bacfc
Aleksey Shapkin <ceylonneck@gmail.com>
2025-12-16 22:21:28 +0500

    feat(SearchEngine): added searching by multiple words

--- Mini-SearchEngine/Basic logic/SearchEngine.cs
+++ Mini-SearchEngine/Basic logic/SearchEngine.cs
@@ -1,4 +1,6 @@
-﻿namespace Mini_SearchEngine.Basic_logic
+﻿using System.Xml.XPath;
+
+namespace Mini_SearchEngine.Basic_logic
 {
     public class SearchEngine
     {
@@ -30,9 +32,9 @@
         }
 
         /// <summary>
-        /// Ищет пересечение токенов в документах по запросу.
+        /// Ищет документы, содержащие все слова запроса.
         /// </summary>
-        /// <param name="query">Запрос, в котором ищется нужное нам слово.</param>
+        /// <param name="query">Запрос, в котором ищется нужное нам слова.</param>
         /// <returns>id документов, в которых был запрос.</returns>
         public HashSet<int> Search(string? query)
         {
@@ -48,7 +50,34 @@
                 return [];
             }
 
-            return index.TryGetValue(query, out HashSet<int>? documents) ? documents : []; 
+            string[] tokens = Tokenizer.GetTokens(query);
+            if (tokens.Length is 0)
+            {
+                return [];
+            }
+
+            if (!index.TryGetValue(tokens[0], out HashSet<int>? documents))
+            {
+                return [];
+            }
+            HashSet<int> result = [.. documents];
+
+            // Пересекаем хешсеты с первым, чтобы выяснить пересечение в документах.
+            for (int i = 1; i < tokens.Length; i++)
+            {
+                if (!index.TryGetValue(tokens[i], out HashSet<int>? currentSet))
+                {
+                    return [];
+                }
+
+                result.IntersectWith(currentSet);
+                if (result.Count is 0)
+                {
+                    return [];
+                }
+            }
+
+            return result;
         }
     }
 }
