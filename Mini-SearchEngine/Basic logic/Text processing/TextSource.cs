c1392a75439c55f9d99d0e6375ee3a3616c03c20
Aleksey Shapkin <ceylonneck@gmail.com>
2025-12-24 20:59:41 +0500

    refactor: text processing classes moved to folder and change XML-comments

---
+++ Mini-SearchEngine/Basic logic/Text processing/TextSource.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace Mini_SearchEngine
+{
+    public static class TextSource
+    {
+        /// <summary>
+        /// Возвращает текст из указанного пути файла.
+        /// </summary>
+        /// <param name="path">Путь к файлу, откуда будет браться текст.</param>
+        public static string? FromFile(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                MessageAssistant.RedMessage("Пустой путь файла!");
+                return null;
+            }
+
+            // Изменяем путь, убирая из него кавычки и изменяя слеши, чтобы просчитать файл и достать из него текст.
+            path = path.Trim().Replace("\"", "").Replace(@"\", "/");
+            if (!File.Exists(path))
+            {
+                MessageAssistant.RedMessage("Неверный путь файла!");
+                return null;
+            }
+
+            return File.ReadAllText(path, encoding: Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// Возвращает входную строку, нужен для архитектуры при работе других классов.
+        /// </summary>
+        /// <param name="text">Текст, который будет возвращён.</param>
+        /// <returns>Вводимый текст.</returns>
+        public static string? FromString(string text) => text;
+    }
+}
