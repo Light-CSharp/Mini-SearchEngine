73caecb7305501835e2ffbe655c94b87f756e105
Aleksey Shapkin <ceylonneck@gmail.com>
2026-01-31 19:28:55 +0500

    refactor(MessageAssistant): delete 2 methods and added cw to message

--- Mini-SearchEngine/Code helpers/MessageAssistant.cs
+++ Mini-SearchEngine/Code helpers/MessageAssistant.cs
@@ -3,35 +3,13 @@
     public static class MessageAssistant
     {
         /// <summary>
-        /// Выводит сообщение зелёного цвета, нужен для обозначения успешности действия.
-        /// </summary>
-        /// <param name="message">Текст, который будет изменён в цвете.</param>
-        public static void GreenMessage(string message)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(message);
-            Console.ResetColor();
-        }
-
-        /// <summary>
         /// Выводит сообщение красного цвета, нужен для обозначения неудачности действия.
         /// </summary>
         /// <param name="message">Текст, который будет изменён в цвете.</param>
         public static void RedMessage(string message)
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write(message);
-            Console.ResetColor();
-        }
-
-        /// <summary>
-        /// Выводит сообщение синего цвета, нужен вывода различной информации.
-        /// </summary>
-        /// <param name="message">Текст, который будет изменён в цвете.</param>
-        public static void BlueMessage(string message)
-        {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.Write(message);
+            Console.WriteLine(message);
             Console.ResetColor();
         }
     }
